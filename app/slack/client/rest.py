bdf3347e96b5a73c2b1b4e1ba67c89e54c65e829
nonylene <cookie.speaker@gmail.com>
2018-06-08 02:55:22 +0900

    list -> List

--- app/slack/client/rest.py
+++ app/slack/client/rest.py
@@ -1,9 +1,9 @@
 import json
 import urllib.parse
 import urllib.request
-from typing import Optional
+from typing import List
 
-from ..model import SlackConfig, Channel, User
+from ..model import Channel, User
 
 
 class RestClient:
@@ -11,19 +11,7 @@ class RestClient:
     def __init__(self, token: str):
         self.token = token
 
-    def post_message(
-            self,
-            slack_config: SlackConfig,
-            text: str,
-            channel: Optional[str] = None, username: Optional[str] = None, icon_emoji: Optional[str] = None,
-    ) -> None:
-        if channel is None:
-            channel = slack_config.debug_channel
-        if username is None:
-            username = slack_config.default_username
-        if icon_emoji is None:
-            icon_emoji = slack_config.default_icon_emoji
-
+    def post_message(self, text: str, channel: str, username: str, icon_emoji: str) -> None:
         data = {
             "token": self.token,
             "channel": channel,
@@ -37,7 +25,7 @@ class RestClient:
             data=post_data
         )
 
-    def get_channels(self) -> list[Channel]:
+    def get_channels(self) -> List[Channel]:
         """
         https://api.slack.com/methods/channels.list
         """
@@ -49,7 +37,7 @@ class RestClient:
         data = json.loads(res.read().decode())
         return [Channel.from_json(obj) for obj in data['channels']]
 
-    def get_users(self) -> list[Channel]:
+    def get_users(self) -> List[Channel]:
         """
         https://api.slack.com/methods/users.list
         """
