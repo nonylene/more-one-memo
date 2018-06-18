6e073311608becb3f505468fcf5038e6fc255db5
nonylene <cookie.speaker@gmail.com>
2018-06-18 10:08:08 +0900

    Reformat import

--- slack-client.py
+++ slack-client.py
@@ -4,9 +4,9 @@ import signal
 from google.cloud import datastore
 
 import config
-from app.data import put_config, get_config
+from app import data
+from app import forwarder
 from app.data.model import DatastoreConfig
-from app.forwarder import run, load_user_config
 from app.forwarder.model import SlackConfig
 from app.model import UserConfig
 
@@ -16,19 +16,19 @@ def main():
 
     datastore_config = DatastoreConfig(config.DATASTORE_CONFIG_KIND, config.DATASTORE_CONFIG_ID)
     user_config = UserConfig([".*", ], [], [], [])
-    put_config(datastore_client, datastore_config, user_config)
-    print(get_config(datastore_client, datastore_config))
+    data.put_config(datastore_client, datastore_config, user_config)
+    print(data.get_config(datastore_client, datastore_config))
 
     slack_config = SlackConfig(
         config.SLACK_COLLECTOR_TOKEN, config.SLACK_PERSONAL_TOKEN,
         config.SLACK_POST_CHANNEL, config.SLACK_DEBUG_CHANNEL,
         config.SLACK_DEFAULT_USERNAME, config.SLACK_DEFAULT_ICON_EMOJI
     )
-    run(datastore_config, slack_config)
+    forwarder.run(datastore_config, slack_config)
 
 
 def reload(_signal, _frame):
-    load_user_config()
+    forwarder.load_user_config()
 
 
 parser = argparse.ArgumentParser(description='more-one-memo: Slack forwarder.')
