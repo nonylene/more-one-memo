65449554ad4e5a147f492ffcfda3eb67622d7ad3
nonylene <nonylene@gmail.com>
2020-02-13 17:14:43 +0900

    temp

--- console/src/App.tsx
+++ console/src/App.tsx
@@ -1,24 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Button from '@material-ui/core/Button';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { Grid } from '@material-ui/core';
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CssBaseline />
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <Typography variant="h6" color="inherit">
+            more-one-memo console
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Container>
+        <Grid container>
+          <Grid item>
+
+            <Button variant="contained" color="primary">
+              Hello!
+        </Button>
+
+          </Grid>
+
+        </Grid>
+      </Container>
     </div>
   );
 }
