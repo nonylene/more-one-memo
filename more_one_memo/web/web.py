fd4052cdfee8c00a7d5682ab1d179f25c5ba7eb2
nonylene <nonylene@gmail.com>
2020-03-01 04:24:02 +0900

    web: Return active channels only

--- more_one_memo/web/web.py
+++ more_one_memo/web/web.py
@@ -1,9 +1,11 @@
 # from flask import Flask, request, jsonify
 import asyncio
+from typing import List
 
 from aiohttp import web
 
 from more_one_memo.slack import RestClient
+import more_one_memo.slack.model as slack_model
 from more_one_memo.web.model import WebConfig, User, Channel
 from more_one_memo.web.db import get_user_config, upsert_user_config, init_db, close_db
 from more_one_memo.model import UserConfig
@@ -39,29 +41,32 @@ async def post_config(request: web.Request):
 
 @routes.get('/api/slack/channels')
 async def slack_channels(request: web.Request):
-    channels = []
+    api_channels: List[slack_model.Channel] = []
     next_cursor = None
     while True:
         conversations = await get_slack_client(app).get_public_channels(cursor=next_cursor)
-        channels.extend([Channel.from_api(c) for c in conversations.channels])
+        api_channels.extend(conversations.channels)
         if not conversations.response_metadata.next_cursor:
             break
         await asyncio.sleep(0.5)
         next_cursor = conversations.response_metadata.next_cursor
+    # Return active channels only
+    channels = map(Channel.from_api, filter(lambda c: not c.is_archived, api_channels))
     return web.json_response([c.to_dict() for c in channels])
 
 
 @routes.get('/api/slack/users')
 async def slack_users(request: web.Request):
-    users = []
+    api_users: List[slack_model.User] = []
     next_cursor = None
     while True:
-        api_users = await get_slack_client(app).get_users(cursor=next_cursor)
-        users.extend([User.from_api(u) for u in api_users.members])
-        if not api_users.response_metadata.next_cursor:
+        slack_users = await get_slack_client(app).get_users(cursor=next_cursor)
+        api_users.extend(slack_users.members)
+        if not slack_users.response_metadata.next_cursor:
             break
         await asyncio.sleep(0.5)
         next_cursor = api_users.response_metadata.next_cursor
+    users = map(User.from_api, api_users)
     return web.json_response([u.to_dict() for u in users])
 
 
